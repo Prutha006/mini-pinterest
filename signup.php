c4caf0fa953401c9d00ed6e64edd8a692851d15c
Gohil Urvashiba Balubha <241405735+deadinside55@users.noreply.github.com>
2026-04-25 22:01:34 +0530

    updated signup page

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $connect = new mysqli("localhost", "root", "", "mini_pinterest");
 
 if ($connect->connect_error) {
@@ -12,13 +13,26 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     die("Invalid email format. Please re-enter your email.");
 }
 $password = $_POST['password'];
+  $check = "SELECT * FROM users WHERE email='$email' AND username='$username'";
+    $res = $connect->query($check);
+ if($res->num_rows > 0){
+        echo "Account already exist!";
+ }
+else{
 
 $sql = "INSERT INTO users (username, email, password) 
         VALUES ('$username', '$email', '$password')";
+    
 
 if ($connect->query($sql) === TRUE) {
-    echo "You have succesfully signed up!!";
+    $user_id=$connect->insert_id;
+    $_SESSION['user_id'] = $user_id;
+    header("location:home_page.php");
+    exit();
+
 } else {
-    echo "Error: " . $connect->error;
+     echo "Error: " . $connect->error;
+  }
 }
+
 ?>
