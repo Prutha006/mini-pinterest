063098aeccd5a608a5810e567548b9d359c0de69
Gohil Urvashiba Balubha <241405735+deadinside55@users.noreply.github.com>
2026-04-28 17:04:35 +0530

    Refactor board display logic and update links

--- board.php
+++ board.php
@@ -29,8 +29,8 @@
 
 <div class="navbar">
     <a href="home_page.php">Home</a>
-    <a href="saved.php" class="active">boards</a>
-    <a href="upload.php">+</a>
+    <a href="search.php">Search</a>
+    <a href="saved.php">Saved</a>
     <a href="profile.php">Profile</a>
 </div>
 
@@ -40,23 +40,29 @@ session_start();
 $user_id=$_SESSION['user_id'] ?? 1;
 include "db.php";
 
+$board_id = $_GET['id'] ?? 0;
+$board_sql = "SELECT name FROM boards WHERE id = $board_id AND user_id = $user_id";
+$board_res = $connect->query($board_sql);
 
-$sql = "SELECT * FROM boards WHERE user_id = $user_id";
+
+if ($board_res && $board_res->num_rows > 0) {
+    $board_row = $board_res->fetch_assoc();
+    echo "<h1>" . $board_row['name'] . "</h1>";
+} else {
+    echo "<h1>No board found !</h1>";
+}
+
+$sql = "SELECT posts.image FROM saved_pins JOIN posts ON saved_pins.post_id = posts.id WHERE board_id = $board_id AND user_id = $user_id";
 $result = $connect->query($sql);
-?>
 
-<h1>Your Boards</h1>
-
-<?php
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "<a href='board.php?id=" . $row['id'] . "'>" . $row['name'] . "</a><br>";
+        echo "<img src='" . $row['image'] . "' alt='Pin Image'width='200' style='margin:10px'> ";
     }
 } else {
-    echo "You haven't created any boards yet! Please create one ..!🌸 ";
+    echo "No pins yet";
 }
 ?>
 </div>
-
 </body>
 </html>
