ef5dd057c89a35a346b43757a896c0452cda4ed9
Gohil Urvashiba Balubha <241405735+deadinside55@users.noreply.github.com>
2026-04-28 17:06:22 +0530

    Fix variable name for database connection

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$conn = new mysqli("127.0.0.1", "root", "", "mini_pinterest");
+$connect= new mysqli("127.0.0.1", "root", "", "mini_pinterest");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
 }
 ?>
