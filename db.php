70c3a3bf85ee9cbf80381bc759e8dac965f8917e
Prutha Makwana. <pruthamakwana6@gmail.com>
2026-04-28 23:23:18 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$connect= new mysqli("127.0.0.1", "root", "", "mini_pinterest");
+$conn = new mysqli("localhost", "root", "", "mini_pinterest");
 
-if ($connect->connect_error) {
-    die("Connection failed: " . $connect->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
